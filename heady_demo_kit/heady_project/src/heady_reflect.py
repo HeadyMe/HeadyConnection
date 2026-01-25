54f334276f34537049942ff2c7c589b580d7f86d
HeadyConnection <e@headyconnection.com>
2026-01-25 01:20:58 -0700

    Delete heady_demo_kit directory
    Signed-off-by: HeadyConnection <e@headyconnection.com>

--- heady_demo_kit/heady_project/src/heady_reflect.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import random
-class HeadyReflect:
-    def __init__(self): pass
-    def validate_intent(self, ctx): return True
--- heady_demo_kit/heady_project/src/heady_verticals.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-class VerticalTemplate:
-    @staticmethod
-    def get_config(v): return {"vertical": v}
